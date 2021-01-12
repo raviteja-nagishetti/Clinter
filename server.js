a12dcd1f1059a7368f9b657b7ee0fb119efbca7d
Raviteja <44426733+raviteja-nagishetti@users.noreply.github.com>
2021-01-12 07:12:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ var passport = require('passport');
 const port = process.env.PORT || 3000
 
 
-//app.get('/', (req, res) => res.sendFile(__dirname+'/dist/Twitter/index.html'));
-//app.use(express.static(__dirname+'/dist/Twitter'));
+app.get('/', (req, res) => res.sendFile(__dirname+'/dist/Twitter/index.html'));
+app.use(express.static(__dirname+'/dist/Twitter'));
 
 // ************************ DB Connection ************************
 var dbOptions = {
